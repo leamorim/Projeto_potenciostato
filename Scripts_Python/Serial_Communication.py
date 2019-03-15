bac5f1ed3030f1c1a2f976fcc23a0b7a00f5851f
Lucas Amorim <lea@cin.ufpe.br>
2019-03-15 10:15:01 -0300

    Adição do range_current como parâmetro a ser enviado e retirada da parte de receber fundo de escala

--- Scripts_Python/Serial_Communication.py
+++ Scripts_Python/Serial_Communication.py
@@ -5,7 +5,7 @@ Created on Wed Feb 27 12:27:39 2019
 Este Script Python faz os seguintes procedimentos;
 
     1) Envia para o esp os parâmetros usado no DPV
-        Envio na forma: PARAM_01/PARAM_02/PARAM_03/PARAM_04/PARAM_05/e onde 'e' sinaliza o fim do envio
+        Envio na forma: range_current/V_inicio/V_fim/V_pulso/V_passo/T_pulso/s_rate/e onde 'e' sinaliza o fim do envio
     2) Recebe os valores de corrente medidos
         X --> Valor float referente a coordenada X
         Y --> Valor inteiro referente a corrente faradaica/capacitiva
@@ -23,7 +23,7 @@ import serial, time
 
 esp = serial.Serial('COM4',115200)
 print(esp)
-#time.sleep(1)
+time.sleep(1)
 def start_send():
     a = esp.write(bytes("start".encode()))
     print(a)
@@ -59,16 +59,19 @@ def start_receive():
 
 start_send()
 
+range_current = 1
 V_inicio = -0.6
 V_fim = 0.0
 V_pulso = 0.01
 V_passo = 0.005
 T_pulso = 0.01
+s_rate = 0.2
 
-data_to_send = ascii(V_inicio)+"/"+ascii(V_fim)+"/"+ascii(V_pulso)+"/"+ascii(V_passo)+"/"+ascii(T_pulso)+"/"+"e"
+data_to_send = ascii(range_current)+"/"+ascii(V_inicio)+"/"+ascii(V_fim)+"/"+ascii(V_pulso)+"/"+ascii(V_passo)+"/"+ascii(T_pulso)+"/"+ascii(s_rate)+"/"+"e"
 ret = esp.write(bytes(data_to_send.encode()))
 print(ret)
 end_receive()
+
 
 ''' LEITURA DOS VALORES DO DPV'''
 y2 = np.array([])#criando array vazio que vai receber o Y2 (ou I2) do ESP
@@ -88,37 +91,17 @@ while(1): #Loop que armazena os dados e encerra ao receber o 'end'
         else:
             x = np.concatenate((x,np.array(float(bytes.decode(data)))),axis = None)
 
-    data1 = esp.readline()[:-2]#Corrente faradaica
+    data1 = esp.readline()[:-2]#Corrente faradaica ou capacitiva
     if data1:
         y = np.concatenate((y,np.array(int(bytes.decode(data1)))),axis = None)
 
 
-    data2 = esp.readline()[:-2]#Corrente capacitiva  
+    data2 = esp.readline()[:-2]#Corrente faradaica ou capacitiva  
     if data2:
         y1 = np.concatenate((y1,np.array(int(bytes.decode(data2)))),axis = None)
         y2 = np.concatenate((y2,np.array(int(bytes.decode(data2)) - int(bytes.decode(data1)))),axis = None)
-
-#Recebendo os valores do ajuste de fundo de escala
-start_receive()
-
-while(1): #Loop que armazena os dados e encerra ao receber o 'end'    
-    data = esp.readline()[:-2] #Coordenada X
-    if data:
-        if bytes.decode(data).find("end") != -1:
-            esp.write(bytes("end".encode()))
-            print("ACK end OK")
-            break
-        else:
-            zero = int(bytes.decode(data))
         
-    data = esp.readline()[:-2]#Fundo de escala positivo
-    if data:
-         f_positivo = int(bytes.decode(data))
-    
-    data = esp.readline()[:-2]#Fundo de escala negativo
-    if data:
-         f_negativo = int(bytes.decode(data))
-
+        
 esp.close()
 
 plt.plot(x,y, color = 'blue',label = 'Curva Y')
